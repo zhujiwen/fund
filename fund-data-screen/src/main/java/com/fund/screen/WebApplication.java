acbac4c50730a0f544200287e9cc43ba7898b8e9
zhujiwen <1756653732@qq.com>
2021-08-27 00:05:17 +0800

    feat: 添加了echarts案例

--- fund-data-screen/src/main/java/com/fund/screen/WebApplication.java
+++ fund-data-screen/src/main/java/com/fund/screen/WebApplication.java
@@ -2,9 +2,7 @@ package com.fund.screen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
---
+++ fund-data-screen/src/main/java/com/fund/screen/biz/controller/FundCtroller.java
@@ -0,0 +1,11 @@
+package com.fund.screen.biz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController("/fund")
+public class FundCtroller {
+
+}
